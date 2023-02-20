64d972000350c92b41a27dcd9facff6480aa8e82
Slava Fomin <slava@fomin.io>
2023-02-20 20:01:04 +0300

    Added exercise 005
    Signed-off-by: Slava Fomin <slava@fomin.io>

---
+++ src/exercise-005.js
@@ -0,0 +1,77 @@
+
+/**
+ * Exercise #5
+ * Write a program to multiple two matrices.
+ */
+
+// const matrix1 = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+// ];
+//
+// const matrix2 = [
+//   [9, 8],
+//   [6, 5],
+//   [3, 2],
+// ];
+
+const matrix1 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const matrix2 = [
+  [9, 8, 7],
+  [6, 5, 4],
+  [3, 2, 1],
+];
+
+const resultMatrix = [];
+
+const rowsLength = matrix1.length;
+const colsLength = matrix2[0].length;
+
+if (rowsLength !== colsLength) {
+  throw new Error(`Incorrect matrix size`);
+}
+
+for (const row of matrix1) {
+
+  const resultRow = [];
+
+  for (let c = 0; c < colsLength; c++) {
+
+    const column = [];
+
+    for (let r = 0; r < matrix2.length; r++) {
+      column.push(matrix2[r][c]);
+    }
+
+    resultRow.push(multiplyVectors(row, column));
+
+  }
+
+  resultMatrix.push(resultRow);
+
+}
+
+resultMatrix.forEach(row => console.log(row));
+
+
+/**
+ * @param {number[]} vector1
+ * @param {number[]} vector2
+ *
+ * @returns {number}
+ */
+function multiplyVectors(vector1, vector2) {
+  if (vector1.length !== vector2.length) {
+    throw new Error(`Incorrect vector size`);
+  }
+  let result = 0;
+  for (let i = 0; i < vector1.length; i++) {
+    result += vector1[i] * vector2[i];
+  }
+  return result;
+}
